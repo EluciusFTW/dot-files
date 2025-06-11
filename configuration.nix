3d59fecb2aa3c1776f7ee47fb8b18db6192b823c
EluciusFTW <guysbuss@gmail.com>
2025-06-11 08:36:00 +0200

    added nix config file

---
+++ configuration.nix
@@ -0,0 +1,144 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # For Surfy Wifi
+  hardware.firmware = [
+    pkgs.linux-firmware
+  ];
+  hardware.enableRedistributableFirmware = true;
+
+  # Tinkering
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  programs.hyprland.enable = true;
+  programs.git = {
+    enable = true;
+    config = {
+      user.name = "EluciusFTW";
+      user.email = "guysbuss@gmail.com";
+      credential.helper = "manager";
+    };
+  };
+  services.dbus.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.wlr.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  security.sudo.wheelNeedsPassword = false;
+  services.greetd.enable = true;
+  services.greetd.settings.default_session = {
+    command = "Hyprland";
+    user = "eluciusftw";
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "eftw-surfy"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.eluciusftw = {
+    isNormalUser = true;
+    description = "Guy Buss";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    dotnet-sdk_9
+    fuzzel
+    git
+    git-credential-manager
+    helix
+    hyprlock
+    kitty
+    fastfetch
+    neovim
+    nushell
+    starship
+    vivaldi
+    vscode
+    waybar
+    wget
+  ];
+
+  fonts.packages = with pkgs; [
+    font-awesome
+    jetbrains-mono
+    hack-font
+    nerd-fonts.jetbrains-mono
+    powerline-fonts
+    powerline-symbols
+    # (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; }
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
