0533ba47c9d1c929558c8194d1752cc24d585140
EluciusFTW <guysbuss@gmail.com>
2025-09-16 14:46:22 +0200

    Removed tilde as eob char

--- stow/nvim/.config/nvim-own/lua/config/options.lua
+++ stow/nvim/.config/nvim-own/lua/config/options.lua
@@ -43,3 +43,5 @@ vim.o.scrolloff = 10 -- Minimal number of screen lines to keep above and below t
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+vim.opt.fillchars:append { eob = ' ' }
