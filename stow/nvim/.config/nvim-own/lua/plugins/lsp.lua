41ecf3074a99fe11c6e5316e37f3dd665bfd5f95
EluciusFTW <guysbuss@gmail.com>
2025-09-16 14:49:42 +0200

    Fixed Typescript LSP config

--- stow/nvim/.config/nvim-own/lua/plugins/lsp.lua
+++ stow/nvim/.config/nvim-own/lua/plugins/lsp.lua
@@ -132,22 +132,7 @@ return {
           end,
         },
       }
-
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
       local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
         -- gopls = {},
@@ -158,15 +143,14 @@ return {
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        angularls = {},
         -- ts_ls = {},
-        --
-        -- roslyn_ls = {
-        --   cmd = { 'Microsoft.CodeAnalysis.LanguageServer' },
-        --   filetypes = { 'cs', 'fs', 'fsx' },
-        --   root_dir = function(fname)
-        --     return require('lspconfig.util').root_pattern('*.sln', '*.csproj', '*.fsproj', '.git')(fname)
-        --   end,
+        vtsls = {},
+        -- typescript = {
+        --   inlayHints = {
+        --     propertyDeclarationTypes = { enabled = true },
+        --     functionLikeReturnTypes = { enabled = true },
+        --   },
         -- },
         lua_ls = {
           -- cmd = { ... },
@@ -174,59 +158,30 @@ return {
           -- capabilities = {},
           settings = {
             Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+              runtime = { version = 'LuaJIT' },
               diagnostics = {
-                globals = { 'vim', 'require' },
-                -- disable = { 'missing-fields' },
+                globals = { 'vim' },
               },
-            },
-            angularls = {},
-            typescript = {
-              inlayHints = {
-                propertyDeclarationTypes = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
+              workspace = {
+                checkThirdParty = false,
+                library = vim.api.nvim_get_runtime_file('', true),
               },
+              telemetry = { enable = false },
             },
           },
         },
       }
 
-      -- Ensure the servers and tools above are installed
-      --
-      -- To check the current status of installed tools and/or manually install
-      -- other tools, you can run
-      --    :Mason
-      --
-      -- You can press `g?` for help in this menu.
-      --
-      -- `mason` had to be setup earlier: to configure its options see the
-      -- `dependencies` table for `nvim-lspconfig` above.
-      --
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
+      for server_name, server_config in pairs(servers) do
+        server_config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server_config.capabilities or {})
+        require('lspconfig')[server_name].setup(server_config)
+      end
+
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
     end,
   },
   {
@@ -242,8 +197,6 @@ return {
       }
       require('roslyn').setup {}
     end,
-    opts = {
-      -- your configuration comes here; leave empty for default settings
-    },
+    opts = {},
   },
 }
