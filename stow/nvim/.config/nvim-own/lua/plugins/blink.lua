d80146eaa2de93e862843b17fc7ea48666759e61
EluciusFTW <guysbuss@gmail.com>
2025-10-16 16:59:14 +0200

    Added friendly-snippets

--- stow/nvim/.config/nvim-own/lua/plugins/blink.lua
+++ stow/nvim/.config/nvim-own/lua/plugins/blink.lua
@@ -18,16 +18,17 @@ return {
         return 'make install_jsregexp'
       end)(),
       dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
+        'rafamadriz/friendly-snippets',
       },
+      config = function()
+        require('luasnip.loaders.from_vscode').lazy_load {
+          include = { 'csharp', 'html', 'angular' },
+        }
+
+        local ls = require 'luasnip'
+        ls.filetype_extend('typescript', { 'angular' })
+        ls.filetype_extend('html', { 'angular' })
+      end,
       opts = {},
     },
     'folke/lazydev.nvim',
@@ -57,7 +58,7 @@ return {
       -- <c-k>: Toggle signature help
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
-      preset = 'default',
+      preset = 'enter',
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -67,12 +68,14 @@ return {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono',
+      documentation = {
+        border = 'rounded',
+      },
     },
 
     completion = {
-      -- By default, you may press `<c-space>` to show the documentation.
-      -- Optionally, set `auto_show = true` to show the documentation after a delay.
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      select_first = true,
     },
 
     sources = {
@@ -83,17 +86,7 @@ return {
     },
 
     snippets = { preset = 'luasnip' },
-
-    -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-    -- which automatically downloads a prebuilt binary when enabled.
-    --
-    -- By default, we use the Lua implementation instead, but you may enable
-    -- the rust implementation via `'prefer_rust_with_warning'`
-    --
-    -- See :h blink-cmp-config-fuzzy for more information
     fuzzy = { implementation = 'lua' },
-
-    -- Shows a signature help window while you type arguments for a function
     signature = { enabled = true },
   },
 }
