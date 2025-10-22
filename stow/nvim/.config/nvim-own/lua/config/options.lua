70bf3e6772f66e5d5ee29052ac1ab070cc512212
EluciusFTW <guysbuss@gmail.com>
2025-10-22 23:36:25 +0200

    Added indenting options

--- stow/nvim/.config/nvim-own/lua/config/options.lua
+++ stow/nvim/.config/nvim-own/lua/config/options.lua
@@ -1,27 +1,17 @@
--- [[ Setting options ]]
 --  For more options, you can see `:help option-list`
 
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.mouse = 'a' -- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.showmode = false -- Don't show the mode, since it's already in the status line
+vim.o.mouse = 'a'
+vim.o.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
-
-vim.o.breakindent = true -- Enable break indent
-vim.o.undofile = true -- Save undo history
-vim.o.ignorecase = true -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.undofile = true
+vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.signcolumn = 'yes' -- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
 vim.o.updatetime = 250 -- Decrease update time
 vim.o.timeoutlen = 300 -- Decrease mapped sequence wait time
-vim.o.splitright = true -- Configure how new splits should be opened
+vim.o.splitright = true
 vim.o.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
@@ -45,3 +35,20 @@ vim.o.scrolloff = 10 -- Minimal number of screen lines to keep above and below t
 vim.o.confirm = true
 
 vim.opt.fillchars:append { eob = ' ' }
+
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.smartindent = true
+vim.o.smarttab = true
+vim.o.autoindent = true
+vim.o.breakindent = true -- Enable break indent
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
