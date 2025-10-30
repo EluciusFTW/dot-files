45d187294037d2c8092e8758a44f1d8a462f70a8
EluciusFTW <guysbuss@gmail.com>
2025-10-30 11:04:11 +0100

    Reconfigured wikis

--- stow/nvim/.config/nvim-own/lua/plugins/neowiki.lua
+++ stow/nvim/.config/nvim-own/lua/plugins/neowiki.lua
@@ -2,12 +2,14 @@ return {
   'echaya/neowiki.nvim',
   opts = {
     wiki_dirs = {
-      { name = 'Work', path = '~/sources/my-synchronization-context/vaults/dev' },
+      { name = 'Work', path = '~/wiki/work' },
       { name = 'Personal', path = '~/sources/my-synchronization-context/vaults/personal' },
+      { name = 'Quizzical', path = '~/sources/my-synchronization-context/vaults/dev' },
     },
   },
   keys = {
-    { '<leader>ww', "<cmd>lua require('neowiki').open_wiki_floating('Work')<cr>", desc = 'Open Work Wiki in Floating Window' },
-    { '<leader>wp', "<cmd>lua require('neowiki').open_wiki_floating('Personal')<cr>", desc = 'Open Personal Wiki in Floating Window' },
+    { '<leader>ww', "<cmd>lua require('neowiki').open_wiki_floating('Work')<cr>", desc = 'Open Work Wiki' },
+    { '<leader>wp', "<cmd>lua require('neowiki').open_wiki_floating('Personal')<cr>", desc = 'Open Personal Wiki' },
+    { '<leader>wq', "<cmd>lua require('neowiki').open_wiki_floating('Quizzical')<cr>", desc = 'Open Quizzical Wiki' },
   },
 }
