82158239d61ddb7043fcf26d7b41596ea2d8e325
EluciusFTW <guysbuss@gmail.com>
2025-11-04 00:01:56 +0100

    Reworked fzf-lua keybindings

--- stow/nvim/.config/nvim-own/lua/plugins/fzf.lua
+++ stow/nvim/.config/nvim-own/lua/plugins/fzf.lua
@@ -7,29 +7,157 @@ return {
     },
   },
   config = function()
-    local fzf = require 'fzf-lua'
-    vim.keymap.set('n', '<leader>fG', fzf.global, { desc = '[F]ind [G]lobally' })
-    vim.keymap.set('n', '<leader><leader>', fzf.buffers, { desc = 'Open Buffers' })
-    vim.keymap.set('n', '<leader>fq', fzf.quickfix, { desc = '[F]ind [Q]uickfix' })
-    vim.keymap.set('n', '<leader>fl', fzf.loclist, { desc = '[F]ind [L]ocation' })
-    vim.keymap.set('n', '<leader>ff', fzf.files, { desc = '[F]ind [F]iles' })
-    vim.keymap.set('n', '<leader>fg', fzf.live_grep, { desc = '[F]ind [G]rep' })
-    vim.keymap.set('n', '<leader>\\', fzf.live_grep, { desc = 'Global Grep' })
-    vim.keymap.set('n', '<leader>fw', fzf.grep_cword, { desc = '[F]ind current [W]ord' })
-    vim.keymap.set('n', '<leader>fj', fzf.jumps, { desc = '[F]ind [J]umps' })
-    vim.keymap.set('n', '<leader>/', fzf.lgrep_curbuf, { desc = 'Fuzzy-find in current buffer' })
-    vim.keymap.set('n', '<leader>fd', fzf.diagnostics_document, { desc = '[F]ind [D]iagnostics in File' })
-    vim.keymap.set('n', '<leader>fD', fzf.diagnostics_workspace, { desc = '[F]ind [D]iagnostics in Workspace' })
-    vim.keymap.set('n', '<leader>fk', fzf.keymaps, { desc = '[F]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>uc', fzf.colorschemes, { desc = 'Colorscheme picker' })
-    -- git
-    vim.keymap.set('n', 'gh', fzf.git_hunks, { desc = '[G]it [H]unks' })
-    vim.keymap.set('n', 'gs', fzf.git_status, { desc = '[G]it [S]tatus' })
-
-    fzf.setup {
+    require('fzf-lua').setup {
       defaults = {
         formatter = 'path.filename_first', -- places file name first
       },
+      winopts = {
+        preview = {
+          layout = 'vertical',
+        },
+      },
     }
   end,
+  keys = {
+    {
+      '<leader>fG',
+      function()
+        require('fzf-lua').global()
+      end,
+      desc = '[F]ind [G]lobally',
+    },
+    {
+      '<leader>ff',
+      function()
+        require('fzf-lua').files()
+      end,
+      desc = '[F]ind [F]iles',
+    },
+    {
+      '<leader>fo',
+      function()
+        require('fzf-lua').oldfiles()
+      end,
+      desc = '[F]ind [Old] files',
+    },
+    {
+      '<leader>fr',
+      function()
+        require('fzf-lua').resume()
+      end,
+      desc = '[F]ind last picker and [R]esume',
+    },
+    {
+      '<leader>fp',
+      function()
+        require('fzf-lua').builtin()
+      end,
+      desc = '[F]ind [B]uilt-in pickers',
+    },
+    {
+      '<leader>fcg',
+      function()
+        require('fzf-lua').live_grep { cwd = vim.fn.stdpath 'config' }
+      end,
+      desc = '[F]ind in nvim [C]onfiguration by [G]rep',
+    },
+    {
+      '<leader>fcf',
+      function()
+        require('fzf-lua').files { cwd = vim.fn.stdpath 'config' }
+      end,
+      desc = '[F]ind in nvim [C]onfiguration the [F]ile',
+    },
+    {
+      '<leader><leader>',
+      function()
+        require('fzf-lua').buffers()
+      end,
+      desc = 'Open Buffers',
+    },
+    {
+      '<leader>fq',
+      function()
+        require('fzf-lua').quickfix()
+      end,
+      desc = '[F]ind [Q]uickfix',
+    },
+    {
+      '<leader>fl',
+      function()
+        require('fzf-lua').loclist()
+      end,
+      desc = '[F]ind [L]ocation',
+    },
+    {
+      '<leader>\\',
+      function()
+        require('fzf-lua').live_grep()
+      end,
+      desc = '[F]ind in global by [G]rep',
+    },
+    {
+      '<leader>fw',
+      function()
+        require('fzf-lua').grep_cword()
+      end,
+      desc = '[F]ind current [W]ord',
+    },
+    {
+      '<leader>fj',
+      function()
+        require('fzf-lua').jumps()
+      end,
+      desc = '[F]ind [J]umps',
+    },
+    {
+      '<leader>/',
+      function()
+        require('fzf-lua').lgrep_curbuf()
+      end,
+      desc = 'Fuzzy-find in curent buffer',
+    },
+    {
+      '<leader>fk',
+      function()
+        require('fzf-lua').keymaps()
+      end,
+      desc = '[F]ind [K]eymaps',
+    },
+    {
+      '<leader>fd',
+      function()
+        require('fzf-lua').diagnostics_document()
+      end,
+      desc = '[F]ind [D]iagnostics in File',
+    },
+    {
+      '<leader>fD',
+      function()
+        require('fzf-lua').diagnostics_workspace()
+      end,
+      desc = '[F]ind [D]iagnostics in Workspace',
+    },
+    {
+      '<leader>uc',
+      function()
+        require('fzf-lua').colorschemes()
+      end,
+      desc = 'Pick colorscheme',
+    },
+    {
+      '<leader>gh',
+      function()
+        require('fzf-lua').git_hunks()
+      end,
+      desc = '[G]it [H]unks',
+    },
+    {
+      '<leader>gs',
+      function()
+        require('fzf-lua').git_status()
+      end,
+      desc = '[G]it [S]tatus',
+    },
+  },
 }
