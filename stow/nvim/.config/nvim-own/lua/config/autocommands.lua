5395cdf2dc4f2eb4a754c0a27da53abab0049e91
EluciusFTW <guysbuss@gmail.com>
2026-01-23 17:31:55 +0100

    Added autocommand to delete items from quick list

--- stow/nvim/.config/nvim-own/lua/config/autocommands.lua
+++ stow/nvim/.config/nvim-own/lua/config/autocommands.lua
@@ -11,3 +11,25 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+local function qf_remove_current()
+  local qf = vim.fn.getqflist()
+  local idx = vim.fn.line '.' - 1
+
+  if idx < 0 or idx >= #qf then
+    return
+  end
+
+  table.remove(qf, idx + 1)
+  vim.fn.setqflist(qf, 'r')
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
+  callback = function()
+    vim.keymap.set('n', 'dd', qf_remove_current, {
+      buffer = true,
+      desc = 'Delete quickfix entry',
+    })
+  end,
+})
