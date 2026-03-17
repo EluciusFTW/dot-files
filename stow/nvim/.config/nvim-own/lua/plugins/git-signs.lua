4538f11a13dfb0fb92c657d1a48fa869252f8181
EluciusFTW <guysbuss@gmail.com>
2026-03-17 16:55:44 +0100

    Coupled gitsigns to fzflua picker

--- stow/nvim/.config/nvim-own/lua/plugins/git-signs.lua
+++ stow/nvim/.config/nvim-own/lua/plugins/git-signs.lua
@@ -9,4 +9,54 @@ return {
       changedelete = { text = '~' },
     },
   },
+  config = function(_, opts)
+    require('gitsigns').setup(opts)
+    local gs = require 'gitsigns'
+
+    local actions = {
+      ['Stage hunk'] = gs.stage_hunk,
+      ['Undo stage hunk'] = gs.undo_stage_hunk,
+      ['Reset hunk'] = gs.reset_hunk,
+      ['Preview hunk'] = gs.preview_hunk,
+      ['Blame line'] = gs.blame_line,
+      ['Toggle blame'] = gs.toggle_current_line_blame,
+      ['Diff this'] = gs.diffthis,
+      ['Toggle deleted'] = gs.toggle_deleted,
+      ['Next hunk'] = gs.next_hunk,
+      ['Prev hunk'] = gs.prev_hunk,
+      ['Stage buffer'] = gs.stage_buffer,
+      ['Reset buffer'] = gs.reset_buffer,
+    }
+
+    local function gitsigns_picker()
+      local fzf = require 'fzf-lua' -- lazy require
+
+      local entries = vim.tbl_keys(actions)
+
+      fzf.fzf_exec(entries, {
+        prompt = 'Gitsigns> ',
+        actions = {
+          ['default'] = function(selected)
+            if not selected or not selected[1] then
+              return
+            end
+
+            local choice = selected[1]
+            local fn = actions[choice]
+
+            if fn then
+              vim.schedule(fn)
+            else
+              vim.notify('Unknown Gitsigns action: ' .. choice)
+            end
+          end,
+        },
+      })
+    end
+
+    -- keymap
+    vim.keymap.set('n', '<leader>gp', gitsigns_picker, {
+      desc = 'Gitsigns picker',
+    })
+  end,
 }
--- stow/nvim/.config/nvim-own/lua/plugins/mini.lua
+++ stow/nvim/.config/nvim-own/lua/plugins/mini.lua
@@ -1,18 +1,14 @@
-return { -- Collection of various small independent plugins/modules
+return {
   'echasnovski/mini.nvim',
   lazy = false,
   version = false,
   config = function()
-    -- Better Around/Inside textobjects
-    --
     -- Examples:
     --  - va)  - [V]isually select [A]round [)]paren
     --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
     --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
 
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
@@ -22,14 +18,12 @@ return { -- Collection of various small independent plugins/modules
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
     local statusline = require 'mini.statusline'
-    -- set use_icons to true if you have a Nerd Font
     statusline.setup {
       use_icons = vim.g.have_nerd_font,
       content = {
         active = function()
           local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
           local filename = vim.fn.expand '%:t' -- just the file name
-          local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 1 }
           local location = MiniStatusline.section_location { trunc_width = 75 }
 
           -- Compose your custom statusline
