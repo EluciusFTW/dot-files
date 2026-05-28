0c2fba021a4bf195dd5498876606ddd371b9156e
EluciusFTW <guysbuss@gmail.com>
2026-05-28 09:24:57 +0200

    Added render-markdown

---
+++ stow/nvim/.config/nvim-own/lua/plugins/render-markdown.lua
@@ -0,0 +1,7 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'echasnovski/mini.icons' },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+}
