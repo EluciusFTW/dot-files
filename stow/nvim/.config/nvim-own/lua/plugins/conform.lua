cbb698a6d0844e50e0d47cb118a358a382effe1d
EluciusFTW <guysbuss@gmail.com>
2026-09-21 17:24:53 +0200

    Disabled csharp auto-formatting

--- stow/nvim/.config/nvim-own/lua/plugins/conform.lua
+++ stow/nvim/.config/nvim-own/lua/plugins/conform.lua
@@ -15,10 +15,7 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
+      local disable_filetypes = { c = true, cpp = true, cs = true }
       if disable_filetypes[vim.bo[bufnr].filetype] then
         return nil
       else
@@ -37,7 +34,7 @@ return {
     },
     formatters_by_ft = {
       lua = { 'stylua' },
-      cs = { 'csharpier' },
+      -- cs = { 'csharpier' },
       typescript = { 'prettier' },
       javascript = { 'prettier' },
       html = { 'prettier' },
