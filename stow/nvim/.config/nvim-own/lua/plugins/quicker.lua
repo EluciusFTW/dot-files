28e3c04a2e20d5c4560a18a32b24e9915a95ab1d
EluciusFTW <guysbuss@gmail.com>
2026-09-21 16:56:52 +0200

    Addedd quicker

---
+++ stow/nvim/.config/nvim-own/lua/plugins/quicker.lua
@@ -0,0 +1,12 @@
+return {
+  'stevearc/quicker.nvim',
+  ft = 'qf',
+  ---@module "quicker"
+  ---@type quicker.SetupOptions
+  opts = {},
+  -- config = function()
+  --   vim.keymap.set('n', '<leader>q', function()
+  --     require('quicker').toggle()
+  --   end, 'Toggle quickfix')
+  -- end,
+}
